9b6b4a7a9e10965b226fc51d34b8a32b5e68aa77
Michael <michael2msp@gmail.com>
2022-12-12 15:23:34 -0300

    commit do projeto base validando acesso aprimorar disabled button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+
+const Button = ({ title, onClick}: IButtonProps) => {
+  return <ButtonContainer  onClick={onClick}>
+          {title}
+        </ButtonContainer>;
+};
+
+export default Button;
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    border: 1px solid #81259D;
+    border-radius: 21px;
+    background-color: #81259D;
+    color: #FFF;
+
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+       
+`
